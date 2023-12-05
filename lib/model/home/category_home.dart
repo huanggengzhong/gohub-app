aef5c0cd26e53ecef022e8ae54eebf041d3b8334
罗庚中 <huanggengzhong@daishujiankang.com>
2023-12-05 11:45:44 +0800

    feat:主页数据联调

---
+++ lib/model/home/category_home.dart
@@ -0,0 +1,16 @@
+class CategoryMo {
+  late String name;
+  late int count;
+
+  CategoryMo.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    count = json['count'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['count'] = this.count;
+    return data;
+  }
+}
